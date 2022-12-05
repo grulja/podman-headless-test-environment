7f7b913231d284be3ef85972c2fb69b2944f6774
Jan Grulich <jgrulich@redhat.com>
2022-12-05 14:40:41 +0100

    Add a compose file to use podman-compose

--- mutter/cmd.sh
+++ mutter/cmd.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
+
 # Set DBUS_SESSION_BUS_ADDRESS
 export DBUS_SESSION_BUS_ADDRESS=$DBUS_SOCKET_PATH/bus-$(echo $DISPLAY | tail -c 2)
 
 # Run Mutter
 mutter --wayland --headless --virtual-monitor 1280x720 --no-x11 &
 
+sleep infinity
